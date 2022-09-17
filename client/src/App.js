0394e973c6c57c0398bea98a0346b6cd81b37b6d
tduncan14 <nbatre1@yahoo.com>
2022-09-17 10:59:20 -0700

    keep moving forward

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import "antd/dist/antd.css";
+import { Button, DatePicker, Space, version } from "antd";
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+       <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DefaultLayout from '../../components/DefaultLayout'
+
+function Home() {
+  return (
+    <div>
+      <DefaultLayout>
+         HomePage oafmomoe
+      </DefaultLayout>
+    </div>
+  )
+}
+
+export default Home
