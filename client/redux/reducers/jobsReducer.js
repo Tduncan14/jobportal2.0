a2a631c17763d76d121abc58e7e4cc1bf3948042
tduncan14 <nbatre1@yahoo.com>
2022-10-13 13:25:54 -0700

    keep moving forward

---
+++ client/redux/reducers/jobsReducer.js
@@ -0,0 +1,20 @@
+const intialState =  {
+
+    jobs:[]
+
+
+
+}
+
+
+export const jobsReducer = (state = intialState,action ) => {
+
+
+    switch(action.type)
+    {
+        default: return state
+    }
+
+
+
+}
